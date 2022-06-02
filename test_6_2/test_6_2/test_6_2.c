f7917125a86475c09e850901ceacc6c15998c2b2
exploreranduin <a1249967801@163.com>
2022-06-02 20:59:50 +0800

    6.2

---
+++ test_6_2/test_6_2/test_6_2.c
@@ -0,0 +1,39 @@
+ #define _CRT_SECURE_NO_WARNINGS  1
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	for (int i = 0; i < 2; i++)
+//	{
+//		printf("     **     \n");
+//	}
+//}
+//·´×ª×Ö·û´®
+//void ReverseString(char* a, int size)
+//{
+//	int start = 0;
+//	int end = size - 1;
+//	char tmp = 0;//´æ´¢×Ö·û´®
+//	if (size == 0 || size == 1)
+//	{
+//		a = a;
+//	}
+//	while (start < end)
+//	{
+//		char tmp = a[start];
+//		a[start] = a[end];
+//		a[end] = tmp;
+//		++start;
+//		--end;
+//	}
+//	printf("%s\n", a);
+//}
+//int main()
+//{
+//	int len = 0;
+//	char a[10] = { 0 };//³õÊ¼»¯×Ö·û´®
+//	scanf("%s", &a);
+//	len = strlen(a);
+//	ReverseString(a, len);
+//	return 0;
+//}
