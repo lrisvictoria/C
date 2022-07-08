f97d48d2ec4fef15e5f2d1980f78d18a40215988
exploreranduin <a1249967801@163.com>
2022-07-08 21:18:19 +0800

    7.08 三子棋项目

---
+++ 三子棋/三子棋/game.c
@@ -0,0 +1,135 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include"game.h"
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < col; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			printf(" %c ", board[i][j]);
+			if (j < col - 1)
+				printf("|");
+		}
+		printf("\n");
+		if (i < row - 1)
+		{
+			int j = 0;
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if (j < col - 1)
+					printf("|");
+			}
+			printf("\n");
+		}
+	}
+}
+
+void player_move(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("玩家下棋\n");
+	while (1)
+	{
+		printf("请选择坐标:>");
+		scanf("%d %d", &x, &y);
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("该坐标已有棋子，请重新选择\n");
+			}
+		}
+		else
+		{
+			printf("选择错误，请重新选择\n");
+		}
+	}
+}
+void computer_move(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("电脑下棋\n");
+	while (1)
+	{
+		x = rand() % row;
+		y = rand() % col;
+		if (board[x][y] == ' ')
+		{
+			board[x][y] = '#';
+			break;
+		}
+	}
+}
+static int is_full(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == ' ')
+			{
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+char is_win(char board[ROW][COL], int row, int col)
+{
+	//判断行
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][i] != ' ')
+		{
+			return board[i][1];
+		}
+	}
+	//判断列
+	for (i = 0; i < col; i++)
+	{
+		if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[1][i] != ' ')
+		{
+			return board[1][i];
+		}
+	}
+	//判断主对角线
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+	//判断副对角线
+	if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+	if (is_full(board, row, col) == 1)
+	{
+		return 'Q';
+	}
+	return 'C';
+}
