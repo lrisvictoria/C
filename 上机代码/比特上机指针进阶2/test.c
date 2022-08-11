92ca8a34d931d89242ce00e81dbd702c98e710bd
exploreranduin <a1249967801@163.com>
2022-08-11 19:34:36 +0800

    8.11 指针进阶2

---
+++ 上机代码/比特上机指针进阶2/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+void print(int(*p)[5], int r, int c)//数组指针接收
+{
+	int i = 0;
+	for (i = 0; i < r; i++)
+	{
+		int j = 0;
+		for (j = 0; j < c; j++)
+		{
+			printf("%d ", *(*(p + i) + j));
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+	print(arr, 3, 5);
+}
