1945f062e720ff1bbd060dc8d2ac6b105fbb9697
exploreranduin <a1249967801@163.com>
2022-08-18 20:25:50 +0800

    8.18 指针面试题

---
+++ 日常代码/指针进阶5/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+
+//int main()
+//{
+//	int a[5] = { 1, 2, 3, 4, 5 };
+//	int* ptr = (int*)(&a + 1);
+//	printf("%d,%d", *(a + 1), *(ptr - 1));//2 5
+//	return 0;
+//}
+
+//由于还没学习结构体，这里告知结构体的大小是20个字节
+struct Test
+{
+	int Num;
+	char* pcName;
+	short sDate;
+	char cha[2];
+	short sBa[4];
+}*p;
+//假设p 的值为0x100000。 如下表表达式的值分别为多少？
+//已知，结构体Test类型的变量大小是20个字节
+int main()
+{
+	p = (struct Test*)0x10000000;
+	printf("%p\n", p + 0x1);
+	printf("%p\n", (unsigned long)p + 0x1);
+	printf("%p\n", (unsigned int*)p + 0x1);
+	return 0;
+}
