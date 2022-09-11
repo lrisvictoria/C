4f251ddd844ed89cb67aabacacf894d760c44c65
exploreranduin <a1249967801@163.com>
2022-09-11 20:18:54 +0800

    9.11 新的模拟

--- 日常代码/字符串和内存函数上/test.c
+++ 日常代码/字符串和内存函数上/test.c
@@ -434,35 +434,59 @@
 //	return 0;
 //}
 
-int my_strncmp(char* str1, char* str2, size_t count)
+//int my_strncmp(char* str1, char* str2, size_t count)
+//{
+//	assert(str1 && str2);
+//	while (!((*str1 - *str2)) && *str1 && *str2 && --count)//如果两者相等且不为'\0',并且只操作count个数，
+//	//这里需要前置--，如果使用后置--，那么还会多进入一次循环，导致count多执行一次
+//	//无序进行特殊处理，当前情况直接返回即可
+//	{
+//		str1++;
+//		str2++;
+//	}
+//	return *str1 - *str2;//无论是\0还是其他情况都能处理
+//}
+//int main()
+//{
+//	char arr1[] = "abcd";
+//	char arr2[] = "abcq";
+//	int ret = my_strncmp(arr1, arr2, 3);
+//
+//	if (ret > 0)
+//	{
+//		printf("arr1 > arr2");
+//	}
+//	else if (ret < 0)
+//	{
+//		printf("arr1 < arr2");
+//	}
+//	else
+//	{
+//		printf("arr1 = arr2");
+//	}
+//	return 0;
+//}
+
+char* my_strncat(char* dest, char* src, size_t count)
 {
-	assert(str1 && str2);
-	while (!((*str1 - *str2)) && *str1 && *str2 && --count)//如果两者相等且不为'\0',并且只操作count个数，
-	//这里需要前置--，如果使用后置--，那么还会多进入一次循环，导致count多执行一次
-	//无序进行特殊处理，当前情况直接返回即可
+	char* start = dest;
+	while (*dest)//找目标字符串的\0
 	{
-		str1++;
-		str2++;
+		dest++;
 	}
-	return *str1 - *str2;//无论是\0还是其他情况都能处理
+	while (count--)//count为真
+	{
+		if ((*dest++ = *src++) == 0)//操作长度大于源字符串，不进行补\0，直接返回
+			return(start);
+	}
+	*dest = '\0';//追加完毕没有返回，则手动加上\0,此时\0经过++指向追加元素最后元素的后一个位置
+	return (start);
 }
 int main()
 {
-	char arr1[] = "abcd";
-	char arr2[] = "abcq";
-	int ret = my_strncmp(arr1, arr2, 3);
-
-	if (ret > 0)
-	{
-		printf("arr1 > arr2");
-	}
-	else if (ret < 0)
-	{
-		printf("arr1 < arr2");
-	}
-	else
-	{
-		printf("arr1 = arr2");
-	}
+	char arr1[20] = "hello ";
+	char arr2[] = "worldld";
+	char* ret = my_strncat(arr1, arr2, 5);
+	printf("%s\n", ret);
 	return 0;
 }
