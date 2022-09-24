0af095fdb2355e3a805d98947082d58b3d9827cd
exploreranduin <a1249967801@163.com>
2022-09-24 23:40:57 +0800

    9.24 动态内存下

---
+++ 上机代码/动态内存下_code/test.c
@@ -0,0 +1,186 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h> 
+
+//动态内存常见的错误
+
+//对NULL指针进行解引用操作
+//int main()
+//{
+//	int* p = (int*)malloc(INT_MAX);
+//	if (p == NULL)
+//	{
+//		perror("malloc");
+//		return 1;
+//	}
+//	else
+//	{
+//		*p = 5;
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
+//对动态开辟的空间进行越界访问
+//int main()
+//{
+//	int* p = (int*)malloc(20);
+//	if (p == NULL)
+//	{
+//		return 1;
+//	}
+//	//使用
+//	int i = 0;
+//	for (i = 0; i < 20; i++)//把20当做元素个数了
+//	{
+//		*(p + i) = i;//严重越界
+//	}
+//	//释放
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
+//对非动态内存开辟的空间内存释放
+
+//int main()
+//{
+//	int num = 10;
+//	int* p = &num;
+//	
+//	//...
+//	free(p);
+//	p = NULL;
+//
+//	return 0;
+//}
+
+//使用free释放动态内存的一部分
+//int main()
+//{
+//	int* p = (int*)malloc(40);
+//	if (p == NULL)
+//	{
+//		return 1;
+//	}
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		*p = i;
+//		p++;//p改变了,p不再指向动态开辟内存的起始位置
+// 
+//	}
+//	//释放
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
+//对同一块内存多次释放
+//int main()
+//{
+//	int* p = malloc(40);
+//	if (p == NULL)
+//	{
+//		return 1;
+//	}
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//	free(p);//已经释放过了
+//	p = NULL;
+//	//...继续写代码
+//
+//	free(p);//忘记已经释放过了
+//	return 0;
+//}
+
+//内存泄漏
+//int* get_memory()
+//{
+//	int* p = (int*)malloc(40);
+//	//...
+//	return p;
+//}
+//int main()
+//{
+//	int* ptr = getmemory();
+//	//使用
+//
+//	//没释放
+//	return 0;
+//}
+
+//void GetMemory(char* p)
+//{
+//    p = (char*)malloc(100);
+//}
+//void Test(void)
+//{
+//    char* str = NULL;
+//    GetMemory(str);
+//    strcpy(str, "hello world");
+//    printf(str);
+//}
+//int main()
+//{
+//    Test();
+//    return 0;
+//}
+
+//char* GetMemory(char* p)
+//{
+//    p = (char*)malloc(100);
+//    return p;
+//}
+//void Test(void)
+//{
+//    char* str = NULL;
+//    str = GetMemory(str);
+//    strcpy(str, "hello world");
+//    printf(str);
+//    //释放
+//    free(str);
+//    str = NULL;
+//}
+//int main()
+//{
+//    Test();
+//    return 0;
+//}
+
+//char* GetMemory(void)
+//{
+//    char p[] = "hello world";
+//    return p;
+//}
+//void Test(void)
+//{
+//    char* str = NULL;
+//    str = GetMemory();
+//    printf(str);
+//}
+//
+//int main()
+//{
+//    Test();
+//    return 0;
+//}
+
+int test()
+{
+	int a = 10;
+	return a;
+}
+
+int main()
+{
+	int ret = test();
+	printf("%d\n", ret);
+	return 0;
+}
