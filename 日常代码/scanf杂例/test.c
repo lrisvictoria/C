36cdbbe6bd80f901e002b2bb7123827d0c15dfdb
exploreranduin <a1249967801@163.com>
2022-10-04 15:11:38 +0800

    10.4 三个小样例

---
+++ 日常代码/scanf杂例/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include <stdio.h>
+
+//int main()
+//{
+//	char a;
+//	int c;
+//	scanf("%c%d", &a, &c);
+//	// a读取字符1
+//	// c读取整数23
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[20] = { 0 };
+//	int a = 0;
+//	int ret = scanf("%s%d", arr, &a);
+//
+//	return 0;
+//}
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	scanf("%d", &a, &b);
+	return 0;
+}
