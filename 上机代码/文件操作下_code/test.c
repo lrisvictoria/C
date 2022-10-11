f10969e4d1813a1f1a2ab5623fac42cb852e3b0c
exploreranduin <a1249967801@163.com>
2022-10-12 01:08:57 +0800

    10.12 1/2

--- 上机代码/文件操作下_code/test.c
+++ 上机代码/文件操作下_code/test.c
@@ -36,8 +36,39 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	// 当前文件内容：abcdef
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("fopen()");
+//		return 1;
+//	}
+//	// 读文件
+//	fseek(pf, 3, SEEK_SET);// 从起始位置偏移三个位置读取
+//	int ch = fgetc(pf);// d
+//	printf("%c\n", ch);
+//
+//	fseek(pf, -3, SEEK_END);// 从结尾位置向前偏移3个位置(相当于向后-3个位置)读取
+//	ch = fgetc(pf);//d 
+//	printf("%c\n", ch);
+//
+//	// 经fgetc后，d被读取，文件指针偏移，当前文件指针指向e
+//
+//	fseek(pf, 1, SEEK_CUR);// 从当前位置向后偏移1个位置读取
+//	ch = fgetc(pf);// f
+//	printf("%c\n", ch);
+//
+//	// 关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
 int main()
 {
+	// 当前文件内容：abcdef
 	FILE* pf = fopen("test.txt", "r");
 	if (pf == NULL)
 	{
@@ -45,7 +76,15 @@ int main()
 		return 1;
 	}
 	// 读文件
+	int ch = fgetc(pf);// 读取完指向b
+	ch = fgetc(pf);// 读取完指向c
+	ch = fgetc(pf);// 读取完指向d
 
+	int pos = ftell(pf);// d相对于起始位置偏移量为3
+	printf("%d\n", pos);
+
+	rewind(pf);
+	printf("%d\n", ftell(pf));
 
 	// 关闭文件
 	fclose(pf);
