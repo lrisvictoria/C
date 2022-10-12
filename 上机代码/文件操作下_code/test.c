bf6ce8f72c2fb502ef28605dddb2eba2c6a0f89d
exploreranduin <a1249967801@163.com>
2022-10-12 23:58:03 +0800

    10.12 文件操作下完结

--- 上机代码/文件操作下_code/test.c
+++ 上机代码/文件操作下_code/test.c
@@ -66,28 +66,127 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	// 当前文件内容：abcdef
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("fopen()");
+//		return 1;
+//	}
+//	// 读文件
+//	int ch = fgetc(pf);// 读取完指向b
+//	ch = fgetc(pf);// 读取完指向c
+//	ch = fgetc(pf);// 读取完指向d
+//
+//	int pos = ftell(pf);// d相对于起始位置偏移量为3
+//	printf("%d\n", pos);
+//
+//	rewind(pf);
+//	printf("%d\n", ftell(pf));
+//
+//	// 关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10000;
+//	FILE* pf = fopen("test.txt", "wb");
+//	fwrite(&a, 4, 1, pf);//二进制的形式写到文件中
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+// fgetc
+// 如果读取正常，会返回读取到的字符的ASCII值
+// 如果读取失败，会返回EOF
+
+// fgets
+// 如果读取正常，返回的是存放读取到的数据的地址
+// 如果读取失败，返回的是空指针
+
+// fscanf
+// 如果读取正常，返回的是格式化字符串中指定的数据的个数
+// 如果读取失败，返回的数字小于格式化字符串指定数据的个数
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	int c; // 注意：int，非char，要求处理EOF
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL) 
+//	{
+//		perror("File opening failed");
+//		return 1;
+//	}
+//	//fgetc 当读取失败的时候或者遇到文件结束的时候，都会返回EOF
+//	while ((c = fgetc(pf)) != EOF)
+//	{
+//		putchar(c);
+//	}
+//	//判断是什么原因结束的
+//	if (ferror(pf))// ferror返回值为真，说明遇到了I/O型错误
+//		puts("I/O error when reading");
+//	else if (feof(pf))// feof为真，成功读取到文件尾结束
+//		puts("End of file reached successfully");
+//	
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//#include <stdio.h>
+//enum 
+//{ 
+//	SIZE = 5 
+//};
+//int main(void)
+//{
+//	double a[SIZE] = { 1.,2.,3.,4.,5. };
+//	FILE* fp = fopen("test.bin", "wb"); // 必须用二进制模式
+//	fwrite(a, sizeof * a, SIZE, fp); // 写 double 的数组
+//	fclose(fp);
+//	double b[SIZE];
+//	fp = fopen("test.bin", "rb");
+//	size_t ret_code = fread(b, sizeof * b, SIZE, fp); // 读 double 的数组
+//	if (ret_code == SIZE)// 正常读取
+//	{
+//		puts("Array read successfully, contents: ");
+//		for (int n = 0; n < SIZE; ++n) printf("%f ", b[n]);
+//		putchar('\n');
+//	}
+//	else// 读取不正常
+//	{ 
+//		if (feof(fp))
+//			printf("Error reading test.bin: unexpected end of file\n");
+//		else if (ferror(fp)) 
+//			perror("Error reading test.bin");
+//	}
+//	fclose(fp);
+//}
+
+#include <stdio.h>
+#include <windows.h>
+//VS2013 WIN10环境测试
 int main()
 {
-	// 当前文件内容：abcdef
-	FILE* pf = fopen("test.txt", "r");
-	if (pf == NULL)
-	{
-		perror("fopen()");
-		return 1;
-	}
-	// 读文件
-	int ch = fgetc(pf);// 读取完指向b
-	ch = fgetc(pf);// 读取完指向c
-	ch = fgetc(pf);// 读取完指向d
-
-	int pos = ftell(pf);// d相对于起始位置偏移量为3
-	printf("%d\n", pos);
-
-	rewind(pf);
-	printf("%d\n", ftell(pf));
-
-	// 关闭文件
+	FILE* pf = fopen("test.txt", "w");
+	fputs("abcdef", pf);//先将代码放在输出缓冲区
+	printf("睡眠10秒-已经写数据了，打开test.txt文件，发现文件没有内容\n");
+	Sleep(10000);
+	printf("刷新缓冲区\n");
+	fflush(pf);//刷新缓冲区时，才将输出缓冲区的数据写到文件（磁盘）
+	printf("再睡眠10秒-此时，再次打开test.txt文件，文件有内容了\n");
+	Sleep(10000);
 	fclose(pf);
+	//注：fclose在关闭文件的时候，也会刷新缓冲区
 	pf = NULL;
 	return 0;
 }
---
+++ 日常代码/函数指针杂例/test.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+//#include <stdio.h>
+//
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int main()
+//{
+//	int (*pa[2])(int, int) = { Add , Sub};
+//	printf("%p\n", pa[0]);
+//	printf("%p\n", Add);
+//	printf("%p\n", &Add);
+//
+//	return 0;
+//}
+
+// 函数指针，指向的是一个函数
+// 函数指针数组，每个元素为函数的地址
+// pa是一个函数指针数组，数组元素为Add的地址
+// 那么pa[0]就是函数Add的地址
+// &Add也是函数的地址
+// Add也是函数的地址
+// &只是为了让我们更好理解
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int main()
+//{
+//	//可以存放多个[参数相同返回类型相同]的函数的地址
+//	int (*pfArr[2])(int, int) = { Add,Sub };//把两个函数的地址放入函数指针数组
+//	int ret1 = pfArr[0](2, 3);//pfArr[0]下标访问，拿到Add函数的地址，调用这个函数
+//	int ret2 = pfArr[1](2, 3);//调用Sub函数
+//	int ret3 = (*(pfArr + 0))(2, 3);//pfArr为首元素地址，首元素地址+偏移量+解引用就拿到了数组中的元素
+//	int ret4 = (*pfArr)(2, 3);//也可以
+//	printf("%d\n%d \nd\n", ret1, ret2, ret4);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int* arr[2] = { &a, &b };
+//
+//	printf("%p\n", &a);
+//	printf("%p\n", arr[0]);
+//}
+
+int arr[100];
+
+int main()
+{
+
+	return 0;
+}
